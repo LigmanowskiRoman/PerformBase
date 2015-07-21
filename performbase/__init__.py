62d6aeabb6bf69d488c9e4680579a9729853e8d7
marcin.zasada <marcin.zasada@performgroup.com>
2015-07-21 10:14:32 +0200

    refactor of setup

--- performbase/__init__.py
+++ performbase/__init__.py
@@ -1,1 +1,3 @@
 from performbase.base_test_case import BaseTestCase
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,59 +1,44 @@
+from __future__ import print_function
 from setuptools import setup
-from os import path
+import codecs
+import os
+import re
+
 
 here = path.abspath(path.dirname(__file__))
+def read(file_paths, default=""):
+    # intentionally *not* adding an encoding option to open
+    try:
+        return codecs.open(os.path.join(here, *file_paths), 'r').read()
+    except:
+        return default
+
+
+def find_version(file_paths):
+    version_file = read(file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+description='Reusable Base Test Case Module',
+long_description=read('README.md', default=description)
+
 setup(
     name='performbase',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
-
-    description='Reusable Base Test Case Module',
-    long_description=read('README.md', default=description)
-
-    # The project's main homepage.
+    version=(['performbase', '__init__.py'])
     url='http://gitlab/qa/performbase',
-
-    # Author details
     author='PERFORM',
-    author_email='perform@performgroup.com',
-
-    # Choose your license
     license='Proprietary',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='selenium testing development',
+    packages=find_packages(),
+    install_requires=['selenium>=2.46.0', 'elementium>=1.1.0'],
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: Proprietary',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-    ],
-
-    # What does your project relate to?
-    keywords='selenium testing development',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['selenium>=2.46.0', 'elementium>=1.1.0'],
-
+        ],
 )
