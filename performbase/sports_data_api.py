7dcb17f2c92b4ade870c1125a5ee38c0292fa53d
Marcin Zasada <marcin.zasada@performgroup.com>
2015-08-28 11:12:23 +0200

    removed perform feeds and sdapi from module`

--- performbase/sports_data_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-class SportsDataQueryBuilder(object):
-    @staticmethod
-    def fetch_team_calendar(outletkey, calendar_uuid):
-        url = "http://api.performfeeds.com/soccerdata/match/{}?tmcl={}&_fmt=json".format(outletkey, calendar_uuid)
-        return requests.get(url).json()
