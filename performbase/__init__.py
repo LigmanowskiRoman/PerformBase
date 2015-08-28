43d8fe68aff71139251a58cf3f0ba6627ab1055f
Marcin Zasada <marcin.zasada@performgroup.com>
2015-08-28 11:15:50 +0200

    bump version

--- performbase/__init__.py
+++ performbase/__init__.py
@@ -2,4 +2,4 @@ from performbase.base_test_case import BaseTestCase
 from performbase.perform_feeds_api import *
 from performbase.sports_data_api import *
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
