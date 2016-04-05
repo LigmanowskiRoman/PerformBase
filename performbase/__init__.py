4ce7907824c68014ac0f3337d1f396e0fc8b1610
Marcin Zasada <marcin.zasada@performgroup.com>
2016-04-05 13:49:10 +0200

    hardcoded Selenium version 2.52.0

--- performbase/__init__.py
+++ performbase/__init__.py
@@ -1,3 +1,3 @@
 from performbase.base_test_case import BaseTestCase
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license='Proprietary',
     keywords='selenium testing development',
     packages=find_packages(),
-    install_requires=['selenium', 'elementium>=1.1.0'],
+    install_requires=['selenium==2.52.0', 'elementium>=1.1.0'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
